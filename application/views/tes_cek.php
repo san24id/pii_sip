583cac751cd138d497d81a1ef3745647292fe59b
san24id <nurikhsan175@gmail.com>
2019-12-16 13:51:15 +0700

    delete

--- application/views/tes_cek.php
+++
@@ -1,9 +0,0 @@
-<form method="post" action="action_cek">
-	
-	 <input type="checkbox" name="t3_check[]"  class="checkradio" value="1">&nbsp;Media massa<input type="hidden" name="t3_1txt" value="Media massa"><br>
-      <input type="checkbox" name="t3_check[]"  class="checkradio" value="2">&nbsp;Forum masyarakat/sosialisasi<input type="hidden" name="t3_2txt" value="Forum masyarakat/sosialisasi"><br>
-      <input type="checkbox" name="t3_check[]" id="t3_3"  class="checkradio" value="3">&nbsp;Lainnya <input type="hidden" name="t3_3txt" value="Lainnya"> <input class="form-control input-sm" type="text" name="t3_4txt" id="t3_4txt">
-
-      <input type="submit" name="" value="coba">
-
-</form>
