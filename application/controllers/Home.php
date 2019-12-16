e476868a0093dc94d35fc6bb463ab50421a8c741
san24id <nurikhsan175@gmail.com>
2019-12-16 12:29:16 +0700

    cobain

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1718,6 +1718,8 @@ class Home extends CI_Controller {
 	// 	   redirect('Home/');
 	//    }
 
+	//
+
  		if($_POST['next'] == 'submit'){
  			 if(($_POST['f1'] != '' || $_POST['f1'] != NULL) && ($_POST['f2'] != '' || $_POST['f2'] != NULL) && ($_POST['f3'] != '' || $_POST['f3'] != NULL)  && ($_POST['f4'] != '' || $_POST['f4'] != NULL) && ($_POST['f5'] != '' || $_POST['f5'] != NULL) && ($_POST['f6'] != '' || $_POST['f6'] != NULL) && ($_POST['f7'] != '' || $_POST['f7'] != NULL) && ($_POST['f8'] != '' || $_POST['f8'] != NULL) && ($_POST['f9'] != '' || $_POST['f9'] != NULL) && ($_POST['f10'] != '' || $_POST['f110'] != NULL) && ($_POST['f11'] != '' || $_POST['f11'] != NULL) && ($_POST['f12'] != '' || $_POST['f12'] != NULL)  && ($_POST['f13'] != '' || $_POST['f13'] != NULL) && ($_POST['f14'] != '' || $_POST['f14'] != NULL) && ($_POST['f15'] != '' || $_POST['f15'] != NULL) && ($_POST['f16'] != '' || $_POST['f16'] != NULL) && ($_POST['f17'] != '' || $_POST['f17'] != NULL) && ($_POST['f18'] != '' || $_POST['f18'] != NULL) && ($_POST['f19'] != '' || $_POST['f19'] != NULL) && ($_POST['f20'] != '' || $_POST['f20'] != NULL) && ($_POST['f21'] != '' || $_POST['f21'] != NULL) && ($_POST['f22'] != '' || $_POST['f22'] != NULL) && ($_POST['f23'] != '' || $_POST['f23'] != NULL) && ($_POST['f24'] != '' || $_POST['f24'] != NULL)){
 
