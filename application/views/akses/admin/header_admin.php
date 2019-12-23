07859fa93a7c714eb5b0570ff4dbc69ced6babcf
san24id <nurikhsan175@gmail.com>
2019-12-23 10:50:56 +0700

    logo on super admin

--- application/views/akses/admin/header_admin.php
+++ application/views/akses/admin/header_admin.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <base href="<?php echo base_url() ?>">
   <title>Aplikasi Scanning System</title>
-  <link rel="icon" type="image/png" href="<?=base_url('assets/login/images/sip1.png');?>"/>
+  <link rel="icon" type="image/png" href="<?=base_url('assets/login/images/sip2.png');?>"/>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
