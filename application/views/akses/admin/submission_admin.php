3b594b36629f43f09390a8dc0ef7388a19638864
san24id <nurikhsan175@gmail.com>
2019-12-23 12:26:08 +0700

    modified status pengerjaan

--- application/views/akses/admin/submission_admin.php
+++ application/views/akses/admin/submission_admin.php
@@ -115,12 +115,16 @@
                              echo "On Admin Approval";
                           }else if($row->status == 5){
                              echo "Back To Admin";
-                          }else if($row->status == 6 || $row->status == 7){
-                             echo "Progress on Admin User";                          
+                          }else if($row->status == 6){
+                             echo "Progress on Admin User PB";
+                          }else if($row->status == 7){
+                            echo "Progress on Admin User GC";                                                   
                           }else if($row->status == 8){
                              echo "Finish on Admin User";
-                          }else if($row->status == 9 || $row->status == 10){
-                             echo "Finish on Admin Approval";
+                          }else if($row->status == 9){
+                             echo "Finish on Admin Approval PB";
+                          }else if($row->status == 10){
+                            echo "Finish on Admin Approval GC";
                           }
                         ?>                  
                   </td>
