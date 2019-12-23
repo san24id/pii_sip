bd6f929d933891cd2945cc68a92c91cca70f465e
san24id <nurikhsan175@gmail.com>
2019-12-23 09:07:32 +0700

    change on view master data

--- application/views/akses/admin/submission_admin.php
+++ application/views/akses/admin/submission_admin.php
@@ -115,17 +115,13 @@
                              echo "On Admin Approval";
                           }else if($row->status == 5){
                              echo "Back To Admin";
-                          }else if($row->status == 6){
-                             echo "Progress on Admin User PB";
-                          }else if($row->status == 7){   
-                            echo "Progress on Admin User GC";
+                          }else if($row->status == 6 || $row->status == 7){
+                             echo "Progress on Admin User";                          
                           }else if($row->status == 8){
                              echo "Finish on Admin User";
-                          }else if($row->status == 9){
-                             echo "Finish on Admin Approval PB";
-                          }else if($row->status == 10){
-                            echo "Finish on Admin Approval GC";
-                          }     
+                          }else if($row->status == 9 || $row->status == 10){
+                             echo "Finish on Admin Approval";
+                          }
                         ?>                  
                   </td>
                   <td><?php echo date("d-m-Y H:i:s", strtotime($row->log_update)); ?></td>
